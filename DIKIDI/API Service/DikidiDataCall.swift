c9aa6ab6ddad59c1213f99481010c60da08abc70
AndrewPautov <apautov04@gmail.com>
2021-10-25 19:18:49 +0300

    Add dilidiDataCall file

---
+++ DIKIDI/API Service/DikidiDataCall.swift
@@ -0,0 +1,154 @@
+//
+//  DikidiDataCall.swift
+//  DIKIDI
+//
+//  Created by Андрей Паутов on 23.10.2021.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class DikidiDataCall: ObservableObject {
+    
+    var didChangeHeaderImage = PassthroughSubject<Data, Never>()
+    var didChangeExampleImage = PassthroughSubject<Data, Never>()
+    var didChangeCatalog = PassthroughSubject<[Catalog], Never>()
+    var didChangeCatalogImages = PassthroughSubject<[String: Data], Never>()
+
+    // var didChangedName = PassthroughSubject<Data, Never>()
+
+    var headerImage = Data() {
+        didSet {
+            didChangeHeaderImage.send(headerImage)
+        }
+    }
+    
+    var exampleImage = Data() {
+        didSet {
+            didChangeExampleImage.send(exampleImage)
+        }
+    }
+    
+    var catalog: [Catalog] = [] {
+        didSet {
+            didChangeCatalog.send(catalog)
+        }
+    }
+    
+    var catalogImages: [String: Data] = [:] {
+        didSet {
+            didChangeCatalogImages.send(catalogImages)
+        }
+    }
+    
+//    var catalogImage1 = Data() {
+//        didSet {
+//            didChangeCatalogImage1.send(catalogImage1)
+//        }
+//    }
+    
+//    var nameString = Data() {
+//        didSet {
+//            didChangedName.send(nameString)
+//        }
+//    }
+
+    func getDikidiData() {
+        imageManager()
+        examplesManager()
+        catalogManager()
+        // nameManager()
+    }
+
+   private func getRequest() -> URLRequest? {
+        let url = URL(string: "https://api-beauty.test.dikidi.ru/home/info?")
+        let APIkey = "maJ9RyT4TJLt7bmvYXU7M3h4F797fUKofUf3373foN94q4peAM"
+        
+        guard let url = url else { return nil}
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue(APIkey, forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+       
+       return request
+    }
+    
+    func imageManager(_ completionHeadler: ((ImageModel) -> Void)? = nil) {
+        guard let request = getRequest() else { return }
+        URLSession.shared.dikidiTask(with: request) { [weak self] dikidi, _, _ in
+            guard let self = self else {return}
+            if let dikidi = dikidi {
+                DispatchQueue.main.async {
+                    if let url = URL(string: dikidi.data.image) {
+                        self.headerImage = try! Data(contentsOf: url)
+                    }
+                }
+            }
+        }.resume()
+    }
+
+    func examplesManager(_ completionHeandler: ((ImageModel) -> Void)? = nil) {
+        guard let request = getRequest() else { return }
+        URLSession.shared.dikidiTask(with: request) {[weak self]  dikidi, _, _ in
+            guard let self = self else { return }
+            if let dikidi = dikidi {
+                DispatchQueue.main.async {
+                    if let url = URL(string: dikidi.data.blocks.examples) {
+                        self.exampleImage = try! Data(contentsOf: url)
+                    }
+                }
+            }
+        }.resume()
+    }
+    
+    func catalogManager(_ completionHeandler: (([Catalog]) -> Void)? = nil) {
+        guard let request = getRequest() else { return }
+        URLSession.shared.dikidiTask(with: request) { [weak self] dikidi, _, _ in
+            guard let self = self else { return }
+            if let dikidi = dikidi {
+                DispatchQueue.main.async {
+                    let catalog = dikidi.data.blocks.catalog
+                    for catalogCategory in catalog {
+                        self.catalog.append(catalogCategory)
+                    }
+                    self.catalogImageManager()
+                    guard let completionHeandler = completionHeandler else {return}
+                    completionHeandler(catalog)
+                }
+            }
+        }.resume()
+    }
+    
+    func catalogImageManager(_ completionHeandler: ((ImageModel) -> Void)? = nil) {
+        guard let request = getRequest() else { return }
+        URLSession.shared.dikidiTask(with: request) { [weak self] dikidi, _, _ in
+            guard let self = self else { return }
+            if let dikidi = dikidi {
+                DispatchQueue.main.async {
+                    for catalogDataIndex in 0..<dikidi.data.blocks.catalog.count {
+                        let catalogDataId = dikidi.data.blocks.catalog[catalogDataIndex].id
+                        let catalogDataImageUrl = dikidi.data.blocks.catalog[catalogDataIndex].image.thumb
+                        if let url = URL(string: catalogDataImageUrl) {
+                            self.catalogImages[catalogDataId] = try! Data(contentsOf: url)
+                        }
+                    }
+                }
+            }
+        }.resume()
+    }
+    
+//    func nameManager(_ completionHeandler: ((Text) -> Void)? = nil) {
+//        guard let request = getRequest() else { return }
+//        URLSession.shared.dikidiTask(with: request) { [weak self] dikidi, _, _ in
+//            guard let self = self else { return }
+//            if let dikidi = dikidi {
+//                DispatchQueue.main.async {
+//                    if let url = URL(string: dikidi.data.blocks.catalog[0].name) {
+//                        self.catalogImage = try! Data(contentsOf: url)
+//                    }
+//                }
+//            }
+//        }.resume()
+//    }
+}
